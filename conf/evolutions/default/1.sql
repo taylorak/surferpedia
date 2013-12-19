96ad1b41443beda93d64bdc2f6c856f39088c942
evkomiyama <evkomiyama@gmail.com>
2013-12-18 20:41:05 -1000

    fixed showsurfer formatting

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -10,7 +10,7 @@ create table surfer (
   awards                    varchar(255),
   carousel_url              varchar(255),
   bio_url                   varchar(255),
-  bio                       clob,
+  bio                       longtext,
   type                      varchar(255),
   footstyle                 varchar(255),
   country                   varchar(255),
@@ -20,8 +20,8 @@ create table surfer (
 ;
 
 create table surfer_update (
-  id                        bigint not null,
-  date                      timestamp,
+  id                        bigint auto_increment not null,
+  date                      datetime,
   action                    varchar(255),
   surfer                    varchar(255),
   constraint pk_surfer_update primary key (id))
@@ -32,34 +32,22 @@ create table user (
   first                     varchar(255),
   last                      varchar(255),
   password                  varchar(255),
-  admin                     boolean,
+  admin                     tinyint(1) default 0,
   constraint pk_user primary key (email))
 ;
-
-create sequence surfer_seq;
-
-create sequence surfer_update_seq;
-
-create sequence user_seq;
 
 
 
 
 # --- !Downs
 
-SET REFERENTIAL_INTEGRITY FALSE;
+SET FOREIGN_KEY_CHECKS=0;
 
-drop table if exists surfer;
+drop table surfer;
 
-drop table if exists surfer_update;
+drop table surfer_update;
 
-drop table if exists user;
+drop table user;
 
-SET REFERENTIAL_INTEGRITY TRUE;
-
-drop sequence if exists surfer_seq;
-
-drop sequence if exists surfer_update_seq;
-
-drop sequence if exists user_seq;
+SET FOREIGN_KEY_CHECKS=1;
 
